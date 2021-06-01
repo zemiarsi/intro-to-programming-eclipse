2be8d4210db23564de9a7668c458cc8d8e8f594f
zemiarsi <engbetelehem@yahoo.com>
2021-06-01 16:41:06 -0400

    <Did DOM assignment>

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const today = new Date()
+console.log(today)
+
+const thisyear = today.getFullYear()
+console.log(thisyear)
+
+const footer= document.querySelector('footer')
+console.log(footer)
+
+const copyright= document.createElement('p')
+console .log (copyright)
+
+copyright.innerHTML= `Betelehem &copy ${thisyear}`;
+
+footer.appendChild(copyright)
+
+const skills= [
+    "structural analysis",
+    "problem-solving",
+    "Attention to detail",
+    " Teamwork"
+]
+
+const skillsSection=document.getElementById("skills")
+const skillsList=document.querySelector('ul')
+
+console.log(skills)
+
+for (let i = 0; i < skills.length; i++) {
+    const skill = document.createElement('li');
+
+    skill.innerHTML=skills[i];
+    console.log(skill.innerHTML)
+
+    skillsSection.appendChild(skill)
+}
+
